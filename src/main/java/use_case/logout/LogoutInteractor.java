b22892572230726bde08169843d45ddc6013f127
roy001 <peijie.jin@mail.utoronto.ca>
2025-10-21 11:49:45 -0400

    my work

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -21,6 +21,8 @@ public class LogoutInteractor implements LogoutInputBoundary {
         // * set the current username to null in the DAO
         // * instantiate the `LogoutOutputData`, which needs to contain the username.
         // * tell the presenter to prepare a success view.
+
+
     }
 }
 
